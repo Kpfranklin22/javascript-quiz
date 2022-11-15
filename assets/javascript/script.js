11a5492f18cdc757c7459722c8d765bffeddb904
Kyle Franklin <kpfranklin22@gmail.com>
2022-11-15 14:53:49 -0500

    adding source links to html and questions structure

---
+++ assets/javascript/script.js
@@ -0,0 +1,36 @@
+var questions = [
+  {
+    questionText: "Who invented Javascript?",
+    options: [
+      "Brandon Boyd",
+      "Branden Eich",
+      "Brandon Lee",
+      "Brendan Fletcher",
+    ],
+    correctAnswer: "Branden Eich",
+  },
+
+  {
+    questionText: "What was Javascript originally named?",
+    options: ["Javascript", "Frappuccino", "Javajournal", "Mocha"],
+    correctAnswer: "Mocha",
+  },
+
+  {
+    questionText: "Which one of these is NOT a declaration of a variable?",
+    options: ["var", "scope", "const", "let"],
+    correctAnswer: "scope",
+  },
+
+  {
+    questionText: "Which one of these is NOT an operator?",
+    options: ["assignment", "function", "strict equality", "addition"],
+    correctAnswer: "function",
+  },
+
+  {
+    questionText: "Which one of these is NOT a Javascript interaction?",
+    options: ["alert", "guess", "prompt", "confirm"],
+    correctAnswer: "guess",
+  },
+];
