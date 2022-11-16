94d47d49754d92cd84064e4e7eda2a630f7edcd5
Kyle Franklin <kpfranklin22@gmail.com>
2022-11-15 21:13:35 -0500

    finished functions and added local storage, stuck on high score results

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -38,7 +38,108 @@ var optionOne = document.getElementById("li1");
 var optionTwo = document.getElementById("li2");
 var optionThree = document.getElementById("li3");
 
-var endPage = document.getElementById("endPage");
+var endPoint = document.getElementById("endPoint");
 var gameOverMessage = document.getElementById("gameOver");
 var userInitials = document.getElementById("initialsInput");
 var submitInitialsButton = document.getElementById("enter");
+
+var numberCorrect = 0;
+var currentQuestionIndex = 0;
+var timeLeft = 60;
+var finalScore = "";
+var timerInterval = "";
+var timeAtEnd = "";
+
+function start() {
+  countdown();
+  quizQuestions();
+}
+
+startButton.addEventListener("click", start);
+
+function countdown() {
+  var timer = document.getElementById("timer");
+  timer.style.display = "block";
+  timerInterval = setInterval(function () {
+    timeLeft--;
+    timer.textContent = timeLeft + " seconds left";
+
+    if (timeLeft === 0) {
+      endGame();
+    }
+  }, 1000);
+}
+
+function quizQuestions() {
+  var startingPoint = document.getElementById("startingPoint");
+  startingPoint.style.display = "none";
+  questionArea.style.display = "block";
+  
+  // Loads question text:
+  var currentQuestion = questions[0];
+  questionText.textContent = currentQuestion.questionText;
+  optionOne.textContent = currentQuestion.options[0];
+  optionTwo.textContent = currentQuestion.options[1];
+  optionThree.textContent = currentQuestion.options[2];
+
+  //Event listener for click on answers
+  optionList.addEventListener("click", function (event) {
+    var userAnswer = event.target;
+    var currentQuestion = questions[currentQuestionIndex];
+
+    //Checks to see if the user answer matches the correct answer
+    if (userAnswer.textContent === currentQuestion.correctAnswer) {
+      numberCorrect++;
+    } else {
+      timeLeft = timeLeft - 10;
+    }
+
+    //Changes to the next question
+    currentQuestionIndex++;
+
+    //Changes text to the next question and loads final page if all questions have been answered
+    if (currentQuestionIndex < questions.length) {
+      currentQuestion = questions[currentQuestionIndex];
+      questionText.textContent = currentQuestion.questionText;
+      optionOne.textContent = currentQuestion.options[0];
+      optionTwo.textContent = currentQuestion.options[1];
+      optionThree.textContent = currentQuestion.options[2];
+    } else {
+      endGame();
+    }
+  });
+}
+
+function calculateScore() {
+  finalScore = numberCorrect * 25 + timeLeft;
+}
+
+//Checks to see if there is a high score in local storage and if this user's score is higher. If so, it saves this user's score and initials.
+function checkHighScore() {
+  if (
+    localStorage.getItem("currentHighScore") < finalScore ||
+    localStorage.getItem("currentHighScore" == null)
+  ) {
+    currentHighScore = finalScore;
+    localStorage.setItem("currentHighScore", currentHighScore);
+
+    var currentUserInitials = userInitials.value;
+    localStorage.setItem("userInitials", currentUserInitials);
+  }
+}
+
+// Loads the text for the final page, calculates and displays final score
+function endGame() {
+  calculateScore();
+  clearInterval(timerInterval);
+  questionArea.style.display = "none";
+  endPoint.style.display = "block";
+  gameOverMessage.textContent = "Your final score is " + finalScore + "!";
+
+  //Listens for a click on the button to save user game data
+  submitInitialsButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    checkHighScore();
+    window.open("./highscore.html", "_self");
+  });
+}
---
+++ assets/javascript/script2.js
@@ -0,0 +1,17 @@
+//Global variables
+var highestScore = document.getElementById("highest-score");
+var playAgain = document.getElementById("play-again");
+
+currentHighestScore = "";
+currentHighestScoreInitials = "";
+
+// Checks local storage for current high score data displays results
+if (localStorage.getItem("currentHighScore") !== null) {
+  currentHighestScore = localStorage.getItem("currentHighScore");
+  currentHighestScoreInitials = localStorage.getItem("userInitials");
+
+  highestScore.textContent =
+    currentHighestScoreInitials + " : " + currentHighestScore;
+} else {
+  highestScore.textContent = "No high score yet.";
+}
